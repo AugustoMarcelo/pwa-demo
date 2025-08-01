9d1fc86ca902c2f4881f944f840e3cf36f0f1a15
ksimonassi99x <ksimonassi99x@users.noreply.github.com>
2025-08-01 17:01:52 +0000

    deploy: dd6709d57c32e3bf90eb86f6b90b469821351a37

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "pwa-demo-cache-v1";
-const URLS_TO_CACHE = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icons/99x.svg", "./icons/icon-192.png", "./icons/icon-512.png"];
+const URLS_TO_CACHE = ["./", "./index.html", "./styles.css", "./app.js", "./icons/favicon/manifest.json", "./icons/99x.svg", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
